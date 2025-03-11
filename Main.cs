bd07f2ee780d4d0913f840c88d2dd6f141c035c8
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-11 07:02:45 +0700

    Menambahkan class kodeProduk

---
+++ Main.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Serialization;
+using modul4_103022300100;
+
+public class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string produk = kodeProduk.getKodeProduk(kodeProduk.Produk.Smartphone);
+        Console.WriteLine("Kode Produk adalah: " + produk);
+    }
+}
---
+++ kodeProduk.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022300100
+{
+    public class kodeProduk
+    {
+        public enum Produk { Laptop, Smartphone, Tablet, Headset, Keyboard, 
+            Mouse, Printer, Monitor, Smartwatch, Kamera}
+
+        public static string getKodeProduk(Produk produk)
+        {
+            string[] kodeProduk = { "E100", "E101", "E102", "E103", "E104", 
+                "E105", "E106", "E107", "E108", "E109" };
+
+            return kodeProduk[(int)produk];
+        }
+    }
+}
